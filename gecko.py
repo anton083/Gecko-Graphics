021f102c9c0093f27a90aeb9c9b1ee493d4c1cfa
Periareion <periareion05@gmail.com>
2022-01-29 14:28:49 +0100

    some changes

--- gecko.py
+++ gecko.py
@@ -8,7 +8,7 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 import numpy as np
 
-# Empty color with zero alpha
+# An empty color with zero alpha
 empty = pygame.Color(0,0,0,0)
 
 BACKGROUND_COLOR = pygame.Color(255,255,255)
@@ -17,25 +17,28 @@ HALF_SCREEN = [WIDTH/2, HEIGHT/2]
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Gecko Graphics')
+
 try:
-    pygame.display.set_icon(pygame.image.load('images/gecko3.png'))
+    pygame.display.set_icon(pygame.image.load('images/gecko.png'))
 except:
     pass
 
 screen.fill(BACKGROUND_COLOR)
 pygame.display.update()
 
-path_surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA, 32).convert_alpha()
+path_surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+
+def screen_pos(position):
+    return position*[1,-1]+HALF_SCREEN
 
 def update():
     screen.fill(BACKGROUND_COLOR)
     screen.blit(path_surface, (0,0))
+    
     for gecko in geckos:
         gecko.draw_body()
+        
     pygame.display.update()
-
-def screen_pos(position):
-    return position*[1,-1]+HALF_SCREEN
 
 def draw_line(color, start, end, width=1):
     pygame.draw.line(path_surface, color, start, end, width)
@@ -56,34 +59,35 @@ class Gecko:
         self.pencolor = self.color
         self.pensize = 1
 
-        self.surf = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
         self.trace_gecko = True
         self.show_gecko = True
 
+        # Update screen when turtle moves (will make it slower)
         self.auto_update = True
 
         self.first_position = self.position.copy()
         self.set_last_position()
 
-    @property
-    def properties(self):
-        p = {
-            'position':self.position,
-            'angle':self.angle,
-            'shape':self.shape,
-            'color':self.color,
-            'size':self.size,
-            'pencolor':self.pencolor,
-            'pensize':self.pensize,
-            'trace_gecko':self.trace_gecko,
-            'show_gecko':self.show_gecko,
-            }
-        return p
-
     def _update(self):
 
         if self.auto_update == True:
             update()
+
+    @property
+    def properties(self):
+        p = {
+            'position': self.position,
+            'angle': self.angle,
+            'shape': self.shape,
+            'color': self.color,
+            'size': self.size,
+            'pencolor': self.pencolor,
+            'pensize': self.pensize,
+            'trace_gecko': self.trace_gecko,
+            'show_gecko': self.show_gecko,
+        }
+        
+        return p
 
     def tracing_on(self):
         self.trace_gecko = True
@@ -93,12 +97,7 @@ class Gecko:
         
     def set_last_position(self):
         self.last_position = self.position.copy()
-
-    def draw_path_line(self):
-        if self.trace_gecko:
-            draw_line(self.pencolor, screen_pos(self.last_position), screen_pos(self.position), self.pensize)
-            self.set_last_position()
-
+        
     def show(self):
         self.show_gecko = True
         self._update()
@@ -115,6 +114,15 @@ class Gecko:
         self.pencolor = pygame.Color(color)
         self._update()
 
+    def rotate(self, angle):
+        self.angle += angle
+
+    def left(self, angle):
+        self.rotate(angle)
+
+    def right(self, angle):
+        self.rotate(-angle)
+
     def set_position(self, *position):
         if len(position) == 1:
             position = np.array(*position)
@@ -126,6 +134,7 @@ class Gecko:
         self.draw_path_line()
         self._update()
 
+    # Aliases for set_position
     setpos = set_pos = goto = setposition = set_position
 
     def move(self, *offset):
@@ -139,33 +148,6 @@ class Gecko:
         self.draw_path_line()
         self._update()
 
-    def draw_body(self):
-        if self.show_gecko:
-            self.surf.fill(empty)
-            if self.shape == 'circle':
-                pygame.draw.circle(self.surf, self.color, screen_pos(self.position)+1, self.size)
-            elif self.shape == 'square':
-                pygame.draw.rect(self.surf, self.color, pygame.Rect(screen_pos(self.position)-self.size+1, (2*self.size, 2*self.size)))
-            elif self.shape == 'triangle':
-
-                # These positions form a triangle that points in the direction of the Gecko
-                pos0 = self.position + [6*math.cos(math.radians(self.angle)), 6*math.sin(math.radians(self.angle))]
-                pos1 = self.position + [6*math.cos(math.radians(self.angle+150)), 6*math.sin(math.radians(self.angle+120))]
-                pos2 = self.position + [6*math.cos(math.radians(self.angle+210)), 6*math.sin(math.radians(self.angle+240))]
-                
-                pygame.draw.polygon(self.surf, self.color, [screen_pos(pos0),screen_pos(pos1),screen_pos(pos2)])
-            
-            screen.blit(self.surf, (0,0))
-
-    def rotate(self, angle):
-        self.angle += angle
-
-    def left(self, angle):
-        self.rotate(angle)
-
-    def right(self, angle):
-        self.rotate(-angle)
-
     def forward(self, distance):
         self.move([distance*math.cos(math.radians(self.angle)), distance*math.sin(math.radians(self.angle))])
 
@@ -173,6 +155,31 @@ class Gecko:
         self.move([-distance*math.cos(math.radians(self.angle)), -distance*math.sin(math.radians(self.angle))])
 
     fd, bk = forward, backward
+
+    def draw_body(self):
+        if self.show_gecko:
+            if self.shape == 'circle':
+                pygame.draw.circle(screen, self.color, screen_pos(self.position)+1, self.size)
+            elif self.shape == 'square':
+                pygame.draw.rect(screen, self.color, pygame.Rect(screen_pos(self.position)-self.size+1, (2*self.size, 2*self.size)))
+            elif self.shape == 'triangle':
+
+                # These positions form a triangle that points in the direction of the Gecko
+                pos0 = screen_pos(self.position + [6*math.cos(math.radians(self.angle)), 6*math.sin(math.radians(self.angle))])
+                pos1 = screen_pos(self.position + [6*math.cos(math.radians(self.angle+150)), 6*math.sin(math.radians(self.angle+120))])
+                pos2 = screen_pos(self.position + [6*math.cos(math.radians(self.angle+210)), 6*math.sin(math.radians(self.angle+240))])
+
+                pygame.draw.polygon(screen, self.color, [pos0,pos1,pos2])
+                
+                pygame.draw.line(screen, '#000000', pos0, pos1, 1)
+                pygame.draw.line(screen, '#000000', pos0, pos2, 1)
+                pygame.draw.line(screen, '#000000', pos1, pos2, 2)
+
+    def draw_path_line(self):
+        if self.trace_gecko:
+            draw_line(self.pencolor, screen_pos(self.last_position), screen_pos(self.position), self.pensize)
+            self.set_last_position()
+
 
     def clone(self, name):
         clone = Gecko()
@@ -200,14 +207,26 @@ def q():
 def main():
 
     g = Gecko()
+    g.right(90)
     g.set_color(pygame.Color(255,0,0))
-    g.set_pencolor(pygame.Color(0,255,0))
-
-    for i in range(0):
-        for n in range(360):
-            g.forward(2)
-            g.rotate(1*(2*(i%2)-1))
+    g.set_pencolor(pygame.Color(0,255,255))
+    g.pensize = 2
+    g.auto_update = False
+    test = 0
+    g.hide()
+    for k in range(16383):
+        for i in range(2):
+            if i == 0:
+                g.set_pencolor((0,128-k//128,255-k//64))
+            else:
+                g.set_pencolor((255-k//64,128-k//128,0))
+            for n in range(180):
+                test += 1
+                g.forward((k+1)/3000)
+                g.rotate(2*(2*(i%2)-1))
+        if not k%20:
             update()
+    print(test)
 
 if __name__ == '__main__':
     print("Enter 'q()' to close the window")
--- import_test.py
+++ import_test.py
@@ -1,4 +1,4 @@
-import gecko
+import Gecko.gecko as gecko
 
 g = gecko.Gecko()
 
